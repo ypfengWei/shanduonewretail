e3710d531867daa0023fd66bcd56a7f5fad41e2e
ypfengwei <642514910@qq.com>
2018-07-12 11:15:00 +0800

    解决

--- src/main/java/com/shanduo/newretail/mapper/UserSellerMapper.java
+++ src/main/java/com/shanduo/newretail/mapper/UserSellerMapper.java
@@ -1,11 +1,7 @@
 package com.shanduo.newretail.mapper;
 
 import java.util.List;
-<<<<<<< HEAD
 import java.util.Map;
-=======
->>>>>>> 354341fb34a556905b47185ed34514df4587b7e0
-
 import com.shanduo.newretail.entity.UserSeller;
 
 public interface UserSellerMapper {
@@ -21,9 +17,6 @@ public interface UserSellerMapper {
 
     int updateByPrimaryKey(UserSeller record);
     
-<<<<<<< HEAD
     List<Object> selectNearbySeller(Map<String, Object> params);
-=======
-    List<UserSeller> selectNearbySeller(double lon,double lat);
->>>>>>> 354341fb34a556905b47185ed34514df4587b7e0
+
 }
