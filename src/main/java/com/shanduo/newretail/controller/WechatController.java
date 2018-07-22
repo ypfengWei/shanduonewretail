b6664c803e2295beff9af3ef9161111dcae26983
lixinrong <lixinrongnokia@outlook.com>
2018-07-22 09:23:59 +0800

    ee

--- src/main/java/com/shanduo/newretail/controller/WechatController.java
+++ src/main/java/com/shanduo/newretail/controller/WechatController.java
@@ -11,7 +11,20 @@ import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.sun.org.apache.xalan.internal.xsltc.compiler.Template;
+import org.apache.http.HttpStatus;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,10 +40,7 @@ import com.shanduo.newretail.service.AccessTokenService;
 import com.shanduo.newretail.service.JsApiTicketService;
 import com.shanduo.newretail.util.HttpRequest;
 import com.shanduo.newretail.util.SHA1;
-import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
-import net.sf.json.JSON;
-import net.sf.json.JSONException;
-import net.sf.json.JSONObject;
+
 @Controller
 @RequestMapping(value = "jwechat")
 public class WechatController {
@@ -77,13 +87,13 @@ public class WechatController {
     	AccessToken accessToken = null;
          String requestUrl = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=TOKEN&type=jsapi".replace("TOKEN", access_token);
          // 发起GET请求获取凭证
-         JSONObject jsonObject = HttpRequest.httpsRequest(requestUrl, "GET", null);
+         net.sf.json.JSONObject jsonObject = HttpRequest.httpsRequest(requestUrl, "GET", null);
          if (null != jsonObject) {
              try {
             	 accessToken = new AccessToken();
             	 accessToken.setAccessToken(jsonObject.getString("access_token"));
             	 accessToken.setExpiresIn(jsonObject.getInt("expires_in"));
-             } catch (JSONException e) {
+             } catch (net.sf.json.JSONException e) {
             	 accessToken = null;
                  // 获取token失败
                 // log.error("获取token失败 errcode:{} errmsg:{}", jsonObject.getInt("errcode"), jsonObject.getString("errmsg"));
@@ -131,11 +141,11 @@ public class WechatController {
     public  ResultBean getWXOpenId(String code)  {
          String requestUrl = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=APPID&secret=SECRET&code=CODE&grant_type=authorization_code".replace("APPID", WxPayConsts.APPID).replace("SECRET", WxPayConsts.APPSECRET).replace("CODE", code);
          // 发起GET请求获取凭证
-         JSONObject jsonObject = HttpRequest.httpsRequest(requestUrl, "GET", null);
+        net.sf.json.JSONObject jsonObject = HttpRequest.httpsRequest(requestUrl, "GET", null);
          if (null != jsonObject) {
              try {
             	 return new SuccessBean(jsonObject.getString("openid")) ;
-             } catch (JSONException e) {
+             } catch (net.sf.json.JSONException e) {
             	 return new ErrorBean();
                  // 获取token失败
                 // log.error("获取token失败 errcode:{} errmsg:{}", jsonObject.getInt("errcode"), jsonObject.getString("errmsg"));
@@ -144,25 +154,96 @@ public class WechatController {
          return new ErrorBean();
     }
   //创建自定义菜单
-   /* public ResultBean createMenu() {
+  @ResponseBody
+  @RequestMapping(value = "createMenu")
+    public ResultBean createMenu() {
         try {
             AccessToken token = accessTokenService.selectAccessToken(WxPayConsts.APPID);
             if (token != null) {
                 JSONObject jsonObject = JSON.parseObject(getMenu(token.getAccessToken()));
                 if (jsonObject.containsKey("errcode")) {
                     jsonObject.clear();
-                    Map<String, Object> map = new HashMap<>();
-                    map.put("appid", Config.APPID);
-                    map.put("host", Config.HOST);
-                    Template tpl = freeMarkerConfigurer.getConfiguration().getTemplate("menu.json");
-                    respJSON = JSON.parseObject(WebUtils.createMenu(token.getAccess_token(), FreeMarkerTemplateUtils.processTemplateIntoString(tpl, map)));
+                    JSONObject root=new JSONObject();
+                    JSONArray jsonArray=new JSONArray();
+                    JSONObject child=new JSONObject();
+                    child.put("type","view");
+                    child.put("name","闪多新零售");
+                    child.put("url","https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxe9811ac767f05237&redirect_uri=http://lixinrong.vicp.io/shanduonewretail/index.html&response_type=code&scope=snsapi_userinfo#wechat_redirect");
+                    JSONObject child2=new JSONObject();
+                    child2.put("type","view");
+                    child2.put("name","后台管理");
+                    child2.put("url","http://lixinrong.vicp.io/shanduonewretail/login.html");
+                    jsonArray.add(child);
+                    jsonArray.add(child2);
+                    root.put("button",jsonArray);
+                    return new SuccessBean(createMenu(token.getAccessToken(), root.toJSONString())) ;
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return new SuccessBean() ;
-    }*/
+        return new ErrorBean(10023,"");
+    }
+    /**
+     * 查询自定义菜单
+     */
+    public static String getMenu(String token) {
+        CloseableHttpResponse response = null;
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet method = new HttpGet("https://api.weixin.qq.com/cgi-bin/menu/get?access_token=" + token);
+        try {
+            response = httpClient.execute(method);
+            if (response.getStatusLine().getStatusCode() == org.apache.http.HttpStatus.SC_OK) {
+                return EntityUtils.toString(response.getEntity());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (response != null) {
+                    response.close();
+                }
+                if (httpClient != null) {
+                    httpClient.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+    /**
+     * 创建自定义菜单
+     */
+    public static String createMenu(String token, String menu) {
+        if (menu != null) {
+            CloseableHttpResponse response = null;
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+            HttpPost method = new HttpPost("https://api.weixin.qq.com/cgi-bin/menu/create?access_token=" + token);
+            try {
+                StringEntity stringEntity = new StringEntity(menu, "UTF-8");
+                method.setEntity(stringEntity);
+                response = httpClient.execute(method);
+                if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                    return EntityUtils.toString(response.getEntity());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (response != null) {
+                        response.close();
+                    }
+                    if (httpClient != null) {
+                        httpClient.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
     /*@ResponseBody
 	@RequestMapping(value = "getopenids")
     public  String getMenu(String token) throws IOException {
