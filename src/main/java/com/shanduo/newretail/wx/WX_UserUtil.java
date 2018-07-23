374a62adcdaa08ebf8113e9d50edb1a731498f42
fanshixin <136219541@qq.com>
2018-07-23 16:15:38 +0800

    修改推送和退款回调 Signed-off-by: fanshixin <136219541@qq.com>

--- src/main/java/com/shanduo/newretail/wx/WX_UserUtil.java
+++ src/main/java/com/shanduo/newretail/wx/WX_UserUtil.java
@@ -8,6 +8,7 @@ import com.alibaba.fastjson.JSONObject;
 public class WX_UserUtil {
 
 	private static Logger log = LoggerFactory.getLogger(WX_UserUtil.class);
+	
     /**
      * 根据微信openId 获取用户是否订阅
      * @param openId 微信openId
@@ -17,13 +18,13 @@ public class WX_UserUtil {
         String tmpurl = "https://api.weixin.qq.com/cgi-bin/user/info?access_token="+token+"&openid="+openId;
         JSONObject result = WX_HttpsUtil.httpsRequest(tmpurl, "GET",null);
         JSONObject resultJson = new JSONObject(result);
-        log.error("获取用户是否订阅 errcode:{} errmsg:{}", resultJson.getInteger("errcode"), resultJson.getString("errmsg"));
         String errmsg = (String) resultJson.get("errmsg");
         if(errmsg==null){
             //用户是否订阅该公众号标识（0代表此用户没有关注该公众号 1表示关注了该公众号）。
             Integer subscribe = (Integer) resultJson.get("subscribe");
             return subscribe;
         }
+        log.error("获取用户是否订阅 errcode:{} errmsg:{}", resultJson.getInteger("errcode"), resultJson.getString("errmsg"));
         return -1;
     }
 }
