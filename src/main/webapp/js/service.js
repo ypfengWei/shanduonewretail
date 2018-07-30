abe7e48a47a94f1c67fa66009684cb3cc5d8077d
lixinrong <lixinrongnokia@outlook.com>
2018-07-30 14:09:23 +0800

    e

--- src/main/webapp/js/service.js
+++ src/main/webapp/js/service.js
@@ -80,28 +80,3 @@ function getGoodsAll(token, categoryId, page, pageSize, cbOk, cbErr) {
         }
     });
 }
-/*
-
-function addClassify() {
-    var classifyName = $('#classifyName').val();
-    if ($.trim(classifyName) === '') {
-        toast('填写类别名');
-        return;
-    }
-    $.ajax({
-        url: "/yapingzh/addClassify.do",
-        data: {"commodityClassification.commodityType": "" + classifyName + ""},
-        type: "POST",
-        dataType: "json",
-        success: function (result) {
-            if (result.success) {
-                toast('添加成功');
-                history.go(-1);
-            } else {
-                toast('类别重复');
-            }
-        }, error: function () {
-            toast('未知错误');
-        }
-    });
-}*/
