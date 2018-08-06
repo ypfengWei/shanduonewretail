3f07567ed31b8523d1a9cee593fbdeef8b11cd8e
fanshixin <136219541@qq.com>
2018-08-06 15:59:45 +0800

    添加新的实体类 Signed-off-by: fanshixin <136219541@qq.com>

---
+++ src/main/java/com/shanduo/newretail/mapper/AppUpgradeMapper.java
@@ -0,0 +1,17 @@
+package com.shanduo.newretail.mapper;
+
+import com.shanduo.newretail.entity.AppUpgrade;
+
+public interface AppUpgradeMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(AppUpgrade record);
+
+    int insertSelective(AppUpgrade record);
+
+    AppUpgrade selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(AppUpgrade record);
+
+    int updateByPrimaryKey(AppUpgrade record);
+}
